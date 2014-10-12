190f836c9cb2e2e28dbaa4f9abb2507d3461e207
rob ludwig <rob@crunchbang>
2014-10-11 19:26:26 -0500

    add a unit model

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 #! /usr/bin/python
-
+import os
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+print "using basedir ", basedir
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+db = SQLAlchemy(app)
+
+class Unit(db.Model):
+	__tablename__ = 'units'
+	id = db.Column(db.Integer, primary_key=True)
+	created = db.Column(db.Date)
+	value = db.Column(db.String(1024))
+	name = db.Column(db.String(256), index=True)
+	votes = db.Column(db.Integer)
+
+	def __repr__(self):
+		return "entry %d %s:%s with %d votes" % (id, name, value, votes)
 
 @app.route('/')
 def index():
