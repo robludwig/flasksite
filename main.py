c2c526329603016a1c2f8694caeb983be18e4bba
rob ludwig <rob@crunchbang>
2014-10-11 19:49:39 -0500

    form for unit class

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 #! /usr/bin/python
 import os
+import datetime
+
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 print "using basedir ", basedir
@@ -21,9 +26,26 @@ class Unit(db.Model):
 	def __repr__(self):
 		return "entry %d %s:%s with %d votes" % (id, name, value, votes)
 
+class UnitForm(Form):
+	name = StringField("What's your idea called?", validators=[Required()])
+	idea = StringField("What's short description of your idea?", validators=[Required()])
+	submit = SubmitField('Submit')
+
 @app.route('/')
 def index():
 	return '<h1>Hello World</h1>'
 
+@app.route('/unit/', methods ['GET', 'POST'])
+def unit():
+	form  = UnitForm()
+	if form.validate_on_submit():
+		unit = Unit(name=form.name.data, value = form.idea.data)
+		unit.created = datetime.now()
+		unit.votes = 0
+		db.session.add(unit)
+		return redirect(url_for('index'))
+	else:
+		return render_template('unit.html', form=form)
+	
 if __name__ == '__main__':
 	app.run(debug=True)
