81ecf738449dd87228d5aa9411fc6dbd17547b50
rob ludwig <rob@crunchbang>
2014-10-12 19:01:02 -0500

    error log origin

--- app/main/errors.py
+++ app/main/errors.py
@@ -3,8 +3,10 @@ from . import main
 
 @main.app_errorhandler(404)
 def page_not_found(e):
+	print "error", e
 	return render_template('404.html'), 404
 
 @main.app_errorhandler(500)
 def internal_error(e):
+	print "error", e
 	return render_template('500.html'), 500
