441524f88e61d751bb052de956fd43c751ed1fdc
rob ludwig <rob@crunchbang>
2014-10-12 19:00:53 -0500

    add deploy command

--- manage.py
+++ manage.py
@@ -13,6 +13,13 @@ def make_shell_context():
 	return dict(app=app, db=db, Unit=Unit)
 manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
+@manager.command
+def deploy():
+	"""Run deployment tasks."""
+	from flask.ext.migrate import upgrade
+	from app.models import Unit
+
+	upgrade()
 
 if __name__ == '__main__':
 	manager.run()
